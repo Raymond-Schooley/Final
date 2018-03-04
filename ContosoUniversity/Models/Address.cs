738fc585dfa0611958ace7c0ff0a07a369045d20
Raymond Schooley <ravschoo@uw.edu>
2018-03-03 19:46:37 -0800

    first commit

---
+++ ContosoUniversity/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Address
+    {
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2, 4}")]
+        public string Email { get; set; }
+    }
+}
